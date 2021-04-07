fc104a50d29ad9d451b903486fec2e173caf5009
SAKHR ERROBAI <sakhr.errobai@edu.uca.ma>
2021-04-07 12:20:38 +0100

    model

---
+++ app/src/main/java/com/example/examenandroid/model/Chapitre.kt
@@ -0,0 +1,31 @@
+package com.example.examenandroid.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Chapitre(var id:Int, var nom: String?) :Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(nom)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Chapitre> {
+        override fun createFromParcel(parcel: Parcel): Chapitre {
+            return Chapitre(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Chapitre?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/examenandroid/model/Question.kt
@@ -0,0 +1,33 @@
+package com.example.examenandroid.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Question(var id:Int, var question: String?, var id_chapitre:Int):Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(question)
+        parcel.writeInt(id_chapitre)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Question> {
+        override fun createFromParcel(parcel: Parcel): Question {
+            return Question(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Question?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
