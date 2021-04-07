fa3b70b1b0e2115ae22b0f2c1702ed445f0180e0
LamiaeBoujaafar <lamiae20ber@gmail.com>
2021-04-07 13:26:18 +0200

    adding models

--- app/src/main/java/com/example/examenandroid/model/User.kt
+++ app/src/main/java/com/example/examenandroid/model/User.kt
@@ -1,4 +1,35 @@
 package com.example.examenandroid.model
 
-class User {
+import android.os.Parcel
+import android.os.Parcelable
+
+class User (var id: Int,var nom:String?,var motDePasse:String?,var email:String?):Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(nom)
+        parcel.writeString(motDePasse)
+        parcel.writeString(email)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
